478612e9d1c65b41daffd504e5f34d52cdbf6de8
Endalkachew <44495881+EndalkPro@users.noreply.github.com>
2020-01-28 10:50:57 -0800

    added MEDIA_URL to urls.py for DEBUG=True or development envirounment

--- CodeTopia/urls.py
+++ CodeTopia/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+
 from .views import *
 
 urlpatterns = [
@@ -25,3 +27,5 @@ urlpatterns = [
 
     path('', include('account.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
