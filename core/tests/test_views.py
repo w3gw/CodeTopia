081036c7a48cb778d00ec6893bad11f35fda560c
Endalkachew <44495881+EndalkPro@users.noreply.github.com>
2020-02-03 04:11:34 -0800

    Updated some tests on core app

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,11 +1,20 @@
 
 from django.test import TestCase, Client
 
+from django.urls import reverse
 
 class CoreViewTests(TestCase):
 
     def test_homepage(self):
         """test the index route"""
         client = Client()
-        response = client.get("/")
+        response = client.get(reverse("homepage"))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response=response, template_name="core/index.html", msg_prefix="")
+
+    def test_elements(self):
+        """test the elements route"""
+        client = Client()
+        response = client.get(reverse("elements"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response=response, template_name="core/elements.html", msg_prefix="")
